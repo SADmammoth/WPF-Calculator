851c52e389f7ea61977841955f490dee0917fdf8
SADmammoth <lo.ma200018@gmail.com>
2020-02-05 21:46:40 +0300

    first copy to remote repository

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public StringBuilder Expression { get; } = new StringBuilder();
+
+        public bool AddElement(string element)
+        {
+            bool is_result = false;
+            if(element == "=")
+            {
+                element+=Evaluate();
+                is_result = true;
+            }
+            Expression.Append(element);
+            return is_result;
+        }
+        public double Evaluate(StringBuilder expression = null)
+        {
+            if (expression == null)
+            {
+                expression = Expression;
+            }
+            double result = 0;
+            Regex numreg = new Regex(@"[\d.]+");
+            Match match = numreg.Match(expression.ToString());
+            double newval;
+            result += double.Parse(match.Value);
+
+            while (match.NextMatch().Success)
+            {
+                newval = double.Parse(match.NextMatch().Value);
+                result = Operation(result, newval, expression[match.Index + match.Length]);
+                match = match.NextMatch();
+            }
+            return result;
+        }
+
+        public double Operation(double left, double right, char operation)
+        {
+            double result;
+            switch (operation)
+            {
+                case ('+'):
+                    result = left + right; break;
+                case ('-'):
+                    result = left - right; break;
+                case ('/'):
+                    result = left / right; break;
+                case (':'):
+                    result = left / right; break;
+                case ('*'):
+                    result = left * right; break;
+                case ('x'):
+                    result = left * right; break;
+                default: throw new Exception("Explicit operation char");
+            }
+            return result;
+        }
+
+        public StringBuilder Clear()
+        {
+            return Expression.Clear();
+        }
+    }
+}
---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Calculator;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calc calc = new Calc();
+        bool flag = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button button;
+            for (int i = 0; i < Buttons.Children.Count; i++) {
+                button = Buttons.Children[i] as Button;
+                button.Click += PressButton;
+             }
+            Expression.PreviewTextInput += (sender, e)=> { if(!Validate(sender as TextBox, new Regex(@"(\d[+\/*=-]\d?$)|(^[\d.]+$)|(CLEAR)"))) { e.Handled = true; } };
+        }
+
+        private void PressButton(object sender, EventArgs eventArgs)
+        {
+            noStyle(Expression as TextBox, null);
+            if (this.flag)
+            {
+               this.calc.Clear();
+                Expression.Text = "";
+            }
+            string text = (sender as Button).Content.ToString();
+            Console.WriteLine(text + Expression.Text);
+            if (!Validate(Expression, new Regex(@"(\d[+\/*=-]\d?$)|(^[\d.]+$)|(CLEAR)"), Expression.Text+text))
+            {
+                Error(Expression);
+                return;
+            }
+            if (text == "CLEAR")
+            {
+                this.calc.Clear();
+            }
+            else
+            {
+                this.flag = this.calc.AddElement(text);
+            }
+            Expression.Text = this.calc.Expression.ToString();
+
+        }
+
+        private bool Validate(TextBox sender, Regex regex, string text = "")
+        {
+            if(text == string.Empty)
+            {
+                text = sender.Text;
+            }
+            if (!regex.IsMatch(text))
+            {
+                Error(sender);
+                return false;
+            }
+            return true;
+        }
+
+        private void Error(TextBox obj)
+        {
+            obj.Style = this.FindResource("errorBox") as Style;
+            obj.TextInput += (sender, e) => noStyle(sender as TextBox, e);
+        }
+        private void noStyle(TextBox sender, RoutedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            if (box.Style.Equals(this.FindResource("errorBox")))
+            {
+                Thread.Sleep(500);
+                box.Style = this.FindResource("normalBox") as Style;
+            }
+        }
+    }
+}
